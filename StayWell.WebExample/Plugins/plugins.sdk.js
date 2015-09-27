50ae59da3d18da6cae5a453add5e864ac0560fd4
TimmyC <Tim.Costantino@gmail.com>
2015-09-26 23:19:08 -0600

    Cleaned up some rogue extra files and rogue missing files.

---
+++ StayWell.WebExample/Plugins/plugins.sdk.js
@@ -0,0 +1,22 @@
+﻿var PluginSDK = (function () {
+
+    var _self = {
+        Initialize: Initialize,
+    }
+
+    function Initialize(applicationId) {
+        if (applicationId == null || applicationId == undefined) {
+            console.log("ApplicationId is a required parameter.");
+            return;
+        }
+
+        //Try to load the plugins
+        var divElement = $(".staywell-centers");
+        if (divElement != null) {
+            CenterPlugin.Initialize(divElement, applicationId);
+        }
+    }
+
+    return _self;
+
+}());
